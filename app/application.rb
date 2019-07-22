52dc54447f638c95de7c4cde9daa3ced9110150b
Paul Rosero <paul_rosero@yahoo.com>
2019-07-22 02:14:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,14 +7,15 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
- 
       item_name = req.path.split("/items/").last 
       if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
       else
         resp.status = 400
         resp,write "Item not found"
+      end
     else
+      resp.status = 404
       resp.write song.artist
     end
  
