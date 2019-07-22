11dba9de3dec6ee18b8df75e17bba41f477414f0
Paul Rosero <paul_rosero@yahoo.com>
2019-07-22 02:15:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
         resp.write item.price
       else
         resp.status = 400
-        resp,write "Item not found"
+        resp.write "Item not found"
       end
     else
       resp.status = 404
